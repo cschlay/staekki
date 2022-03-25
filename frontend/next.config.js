8ef2b6ad75c25c74dd7814ee94763060cf9594cb
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-03-25 12:54:24 +0200

    Fix the docker configuration for development purposes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // See https://github.com/vercel/next.js/issues/6417
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
   reactStrictMode: true,
 }
 
